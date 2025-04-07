c2d5c12beabe6696f7828b9f2df6c70a96960072
Kevin McFarlane <kevin.mcfarlane@gmail.com>
2025-04-07 18:14:44 +0100

    Move all script files into wwwroot/pkg and remove Counter and Weather pages

--- BlazorRustWasmApp/wwwroot/pkg/add.js
+++ BlazorRustWasmApp/wwwroot/pkg/add.js
@@ -1,6 +1,6 @@
 ﻿// Key Points:
 //
-// Call __wbg_init: 
+// __wbg_init: 
 // The __wbg_init function initializes the wasm object, making all exported functions from the WebAssembly module accessible.
 // Without this initialization step, wasm will remain undefined.
    
@@ -10,19 +10,18 @@
 // Preload WASM on Page Load: 
 // To improve responsiveness, we attempt to initialize the WebAssembly module as soon as the page loads.
 
-// TODO: move to pkg folder and update index.html
-// import init, * as wasmLib from './pkg/wasm_lib.js';
 import __wbg_init, { add } from './wasm_lib.js';
 
 let wasmInitialized = false;
 
 window.add = async (arg1, arg2) => {
     if (!wasmInitialized) {
-        await __wbg_init(); // Initialize the WASM module.
+        await __wbg_init(); 
         wasmInitialized = true;
         console.log('WASM module initialized');
     }
-    return add(arg1, arg2); // Call the add function once WASM is ready.
+    
+    return add(arg1, arg2); 
 };
 
 // Automatically initialize the WASM module when the page loads.
